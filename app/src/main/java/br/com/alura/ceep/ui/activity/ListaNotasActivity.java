d34a755d7d13b55c456e56e9e276c98239b56d1e
Claudio Costa <claudio@imaginaction.com.br>
2019-10-06 09:40:39 -0300

    feet: 02 - 09 - Implementando o adapter do RecyclerView

--- app/src/main/java/br/com/alura/ceep/ui/activity/ListaNotasActivity.java
+++ app/src/main/java/br/com/alura/ceep/ui/activity/ListaNotasActivity.java
@@ -2,6 +2,8 @@ package br.com.alura.ceep.ui.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutCompat;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
 import java.util.List;
@@ -23,6 +25,8 @@ public class ListaNotasActivity extends AppCompatActivity {
             dao.insere(new Nota("Titulo" + i, "Descrição" + i));
         }
         List<Nota> todosNotas = dao.todos();
-        listaNotas.setAdapter(new ListaNotasAdapter());
+        listaNotas.setAdapter(new ListaNotasAdapter(this, todosNotas));
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        listaNotas.setLayoutManager(layoutManager);
     }
 }
--- app/src/main/java/br/com/alura/ceep/ui/recyclerview/adapter/ListaNotasAdapter.java
+++ app/src/main/java/br/com/alura/ceep/ui/recyclerview/adapter/ListaNotasAdapter.java
@@ -1,34 +1,55 @@
 package br.com.alura.ceep.ui.recyclerview.adapter;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import java.util.List;
 
+import br.com.alura.ceep.R;
 import br.com.alura.ceep.model.Nota;
 
 public class ListaNotasAdapter extends RecyclerView.Adapter {
 
     private List<Nota> notas;
+    private Context context;
 
-    public ListaNotasAdapter(List<Nota> notas){
+    public ListaNotasAdapter(Context context, List<Nota> notas){
+        this.context = context;
         this.notas = notas;
     }
 
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
+        View viewCriada = LayoutInflater.from(context).inflate(R.layout.item_nota, viewGroup, false);
+        return new NotasViewHolder(viewCriada);
     }
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
-
+        Nota nota = notas.get(i);
+        TextView titulo = viewHolder.itemView.findViewById(R.id.item_nota_titulo);
+        titulo.setText(nota.getTitulo());
+        TextView descricao = viewHolder.itemView.findViewById(R.id.item_nota_descricao);
+        descricao.setText(nota.getDescricao());
     }
+
 
     @Override
     public int getItemCount() {
         return notas.size();
     }
+
+    class NotasViewHolder extends RecyclerView.ViewHolder {
+
+        public NotasViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+
 }
