ce9255a5582c9864455fe9e3c60370efd5f1eab4
Claudio Costa <claudio@imaginaction.com.br>
2019-10-05 21:31:55 -0300

    feet: 01 - 08 - Adicionando classes padrões

--- app/src/main/java/br/com/alura/ceep/ui/activity/ListaNotasActivity.java
+++ app/src/main/java/br/com/alura/ceep/ui/activity/ListaNotasActivity.java
@@ -2,8 +2,16 @@ package br.com.alura.ceep.ui.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import java.util.List;
 
 import br.com.alura.ceep.R;
+import br.com.alura.ceep.dao.NotaDAO;
+import br.com.alura.ceep.model.Nota;
+import br.com.alura.ceep.ui.adapter.ListaNotasAdapter;
 
 public class ListaNotasActivity extends AppCompatActivity {
 
@@ -11,5 +19,10 @@ public class ListaNotasActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_notas);
+        ListView listaNotas = findViewById(R.id.listView);
+        NotaDAO dao = new NotaDAO();
+        dao.insere(new Nota("Primeira Nota", "Primeira descrição"));
+        List<Nota> todosNotas = dao.todos();
+        listaNotas.setAdapter(new ListaNotasAdapter(this, todosNotas));
     }
 }
